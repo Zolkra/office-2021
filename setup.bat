9bb25068f0ff38498454e890317dd0e788578265
Zolkra <128638738+Zolkra@users.noreply.github.com>
2023-08-03 12:27:49 +0000

    V1
    Original files

---
+++ setup.bat
@@ -0,0 +1,20 @@
+@echo off
+color D
+echo Installateur Office 2021 + Suite par Zolkra / Office 2021 + Suite Installer by Zolkra
+echo Discord: Zolkra.
+pause
+cls
+
+@echo off
+
+.\setup\setup.exe /configure .\configuration-Office2021Enterprise.xml
+
+cls
+
+color a
+echo Lancement du setup / Launching Setup
+pause
+color 4
+cls
+echo Fin du script !
+pause
